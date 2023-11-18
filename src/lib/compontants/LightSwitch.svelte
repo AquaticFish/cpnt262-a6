219b759d40df1bff15ad1c0cf4536ac99af84d55
Dorothy Nguyen <dorothyjij@gmail.com>
2023-11-17 19:24:49 -0700

    Looks like i forgot to add the repo, so far ive added the nav bar and some how added a VERY COOL light/dark mode swtich

---
+++ src/lib/compontants/LightSwitch.svelte
@@ -0,0 +1,62 @@
+<!-- LightSwitch.svelte -->
+<script>
+  let isOn = false;
+  let isLoading = false;
+
+  const toggleSwitch = () => {
+    isLoading = true;
+
+    // Simulate an asynchronous operation (e.g., fetching data)
+    setTimeout(() => {
+      isOn = !isOn;
+      isLoading = false;
+    }, 1000);
+  };
+</script>
+
+<style>
+  .switch {
+    width: 60px;
+    height: 30px;
+    border-radius: 15px;
+    background: #ddd;
+    position: relative;
+    cursor: pointer;
+  }
+
+  .toggle {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background: #fff;
+    position: absolute;
+    transition: transform 0.3s;
+    transform: translateX(0);
+  }
+
+  .on {
+    transform: translateX(30px);
+  }
+
+  /* Skeleton loading effect */
+  .skeleton {
+    animation: loading 1.5s infinite;
+    background: linear-gradient(90deg, #f0f0f0 25%, #e0e0e0 50%, #f0f0f0 75%);
+    background-size: 200% 100%;
+    border-radius: 15px;
+  }
+
+  @keyframes loading {
+    to {
+      background-position: -200% 0;
+    }
+  }
+</style>
+
+<div on:click={toggleSwitch} class="switch">
+  {#if isLoading}
+    <div class="toggle skeleton"></div>
+  {:else}
+    <div class="toggle" class:{{ on: isOn }}></div>
+  {/if}
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,55 @@
+<script>
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong
+				 class="text-xl uppercase">Hello</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+
+			<LightSwitch />
+
+			<a
+			href="/"
+			class="btn btn-sm variant-ghost-surface"
+			rel="noreferrer"
+		>
+			Home
+		</a>
+			<a
+				  href="/about"
+					class="btn btn-sm variant-ghost-surface"
+					rel="noreferrer"
+				>
+					About
+				</a>
+				<a
+				  href="/gallery"
+					class="btn btn-sm variant-ghost-surface"
+					rel="noreferrer"
+				>
+					Gallery
+				</a>
+				<a
+					href="/contact"
+					class="btn btn-sm variant-ghost-surface"
+					rel="noreferrer"
+				>
+					Contact
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
+
